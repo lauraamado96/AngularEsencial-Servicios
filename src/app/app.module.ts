c9428ac699dbaf02334e835a8d91e69e3d4a6b37
lauraamado96 <laura.amado.96@hotmail.com>
2023-04-28 23:18:26 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,47 @@
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AcercaDeComponent } from './acerca-de/acerca-de.component';
+import { DetallesComponent } from './detalles/detalles.component';
+import { EncabezadoComponent } from './encabezado/encabezado.component';
+import { Error404Component } from './error404/error404.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { ListaDeLibrosComponent } from './lista-de-libros/lista-de-libros.component';
+
+import { LibrosseleccionadosService } from './librosseleccionados.service';
+
+const routes: Routes = [
+  { path: 'lista-libros', component: ListaDeLibrosComponent },
+  { path: 'acerca-de', component: AcercaDeComponent },
+  { path: 'detalles/:libroId', component: DetallesComponent },
+  { path: '', pathMatch: 'full', redirectTo: 'lista-libros' },
+  { path: '404', component: Error404Component },
+  { path: '**', redirectTo: '404' }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AcercaDeComponent,
+    DetallesComponent,
+    EncabezadoComponent,
+    Error404Component,
+    InicioComponent,
+    ListaDeLibrosComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    LibrosseleccionadosService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/lista-de-libros/lista-de-libros.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ListaDeLibrosComponent } from './lista-de-libros.component';
+
+describe('ListaDeLibrosComponent', () => {
+  let component: ListaDeLibrosComponent;
+  let fixture: ComponentFixture<ListaDeLibrosComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ListaDeLibrosComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ListaDeLibrosComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
